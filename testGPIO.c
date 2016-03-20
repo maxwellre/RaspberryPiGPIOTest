885a1d7f603a05ec7dad4b50c23dff49f108c7d9
Maxwellre <yitianshao@gmail.com>
2016-03-20 02:51:39 +0000

    Locally streaming Accelerometer X-Y-Z output is available. Streming to PC is needed for next step.

--- testGPIO.c
+++ testGPIO.c
@@ -20,8 +20,8 @@ void checkRet(int ret, int isDispRet);
 // Main 
 int main(void)
 {
-	int i;
 	int fd, ret;
+	int outX, outY, outZ;
 
 	fd = wiringPiI2CSetup(0x1e);
 	printf("I2C Initialized: ID = %d\n", fd);
@@ -29,18 +29,32 @@ int main(void)
 	pinMode(I2CSDA, OUTPUT);
 	pinMode(I2CSCL, OUTPUT);
 	
+	// CTRL_REG4: ODR = 1600 Hz
 	ret = wiringPiI2CWriteReg8(fd, 0x20, 0x97);
 	checkRet(ret, 0);
 
-	
+	// CTRL_REG5: FSCALE = 8g
+	ret = wiringPiI2CWriteReg8(fd, 0x24, 0x18);
+	checkRet(ret, 0);
 	
 	printf("\n");
 	
 	// The loop --------------------------------------------------------
-	for(i = 0; i < 10; i++)
-	{		
-		ret = wiringPiI2CReadReg8(fd, 0x20);
-		checkRet(ret, 1);	
+	while(1)
+	{	
+		ret = wiringPiI2CReadReg8(fd, 0x27);
+		if( (ret & 0x07) == 0x07)
+		{
+			outX = wiringPiI2CReadReg16(fd, 0x28);
+			outY = wiringPiI2CReadReg16(fd, 0x2A);
+			outZ = wiringPiI2CReadReg16(fd, 0x2C);
+			
+			printf(" %d  %d  %d\n", outX, outY, outZ);
+		}
+		else
+		{
+			checkRet(ret, 0);
+		}	
 	}
 	// -----------------------------------------------------------------
 	
